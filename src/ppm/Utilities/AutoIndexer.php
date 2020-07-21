8d2ad8dec7e9a3eba1fb5aad9fe6e82123da9fcd
netkas <netkas@intellivoid.info>
2020-07-21 18:50:41 -0400

    Added properties RETRY_LIMIT, ignoreDirectories, acceptFiles, autoRebuild, reportParseErrors, scanPaths, excludeDirectories, classes, cacheLoaded, refreshed, missing and cacheDirectory to \ppm\Utilities > AutoIndexer

--- src/ppm/Utilities/AutoIndexer.php
+++ src/ppm/Utilities/AutoIndexer.php
@@ -10,5 +10,38 @@
      */
     class AutoIndexer
     {
+        private const RETRY_LIMIT = 3;
 
+        /** @var string[] */
+        public $ignoreDirs = ['.*', '*.old', '*.bak', '*.tmp', 'temp'];
+
+        /** @var string[] */
+        public $acceptFiles = ['*.php'];
+
+        /** @var bool */
+        private $autoRebuild = true;
+
+        /** @var bool */
+        private $reportParseErrors = true;
+
+        /** @var string[] */
+        private $scanPaths = [];
+
+        /** @var string[] */
+        private $excludeDirs = [];
+
+        /** @var array of class => [file, time] */
+        private $classes = [];
+
+        /** @var bool */
+        private $cacheLoaded = false;
+
+        /** @var bool */
+        private $refreshed = false;
+
+        /** @var array of missing classes */
+        private $missing = [];
+
+        /** @var string|null */
+        private $tempDirectory;
     }
