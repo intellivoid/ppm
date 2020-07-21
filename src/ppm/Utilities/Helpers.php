bdc302189574a2d65493f268e20fa526ab8f1909
netkas <netkas@intellivoid.info>
2020-07-21 19:13:17 -0400

    Added static method Capture() \ppm\Utilities > Helpers

--- src/ppm/Utilities/Helpers.php
+++ src/ppm/Utilities/Helpers.php
@@ -4,11 +4,31 @@
 
     namespace ppm\Utilities;
 
+    use Throwable;
+
     /**
      * Class Helpers
      * @package ppm\Utilities
      */
     class Helpers
     {
-
+        /**
+         * @param callable $func
+         * @return string
+         * @throws Throwable
+         */
+        public static function capture(callable $func): string
+        {
+            ob_start(function () {});
+            try
+            {
+                $func();
+                return ob_get_clean();
+            }
+            catch (Throwable $e)
+            {
+                ob_end_clean();
+                throw $e;
+            }
+        }
     }
