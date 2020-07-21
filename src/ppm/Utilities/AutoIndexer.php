38e1da69116581c199c705295ea9c6784fb9278b
netkas <netkas@intellivoid.info>
2020-07-21 18:48:58 -0400

    Added AutoIndexer to \ppm\Utilities

---
+++ src/ppm/Utilities/AutoIndexer.php
@@ -0,0 +1,14 @@
+<?php
+
+    declare(strict_types=1);
+
+    namespace ppm\Utilities;
+
+    /**
+     * Class AutoIndexer
+     * @package ppm\Utilities
+     */
+    class AutoIndexer
+    {
+
+    }
--- src/ppm/ppm.php
+++ src/ppm/ppm.php
@@ -39,6 +39,7 @@
     include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Traits' . DIRECTORY_SEPARATOR . 'SmartObject.php');
     include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Traits' . DIRECTORY_SEPARATOR . 'StaticClass.php');
 
+    include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Utilities' . DIRECTORY_SEPARATOR . 'AutoIndexer.php');
     include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Utilities' . DIRECTORY_SEPARATOR . 'Autoloader.php');
     include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Utilities' . DIRECTORY_SEPARATOR . 'CLI.php');
     include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Utilities' . DIRECTORY_SEPARATOR . 'DateTime.php');
