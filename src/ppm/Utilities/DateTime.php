27ec127f73c87c7a5a6c31c5d93a54c3d01dad16
netkas <netkas@intellivoid.info>
2020-07-21 18:35:35 -0400

    Added properties MINUTE, HOUR, DAY, WEEK, MONTH & YEAR to \ppm\Utilities > DateTime

--- src/ppm/Utilities/DateTime.php
+++ src/ppm/Utilities/DateTime.php
@@ -10,5 +10,23 @@
      */
     class DateTime extends \DateTime implements \JsonSerializable
     {
+        /** minute in seconds */
+        public const MINUTE = 60;
 
+        /** hour in seconds */
+        public const HOUR = 60 * self::MINUTE;
+
+        /** day in seconds */
+        public const DAY = 24 * self::HOUR;
+
+        /** week in seconds */
+        public const WEEK = 7 * self::DAY;
+
+        /** average month in seconds */
+        public const MONTH = 2629800;
+
+        /** average year in seconds */
+        public const YEAR = 31557600;
+
+        
     }
