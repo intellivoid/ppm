167b1378fa8f4155a2d9e8dc32cfcfc5f484997c
netkas <netkas@intellivoid.info>
2020-07-21 18:11:56 -0400

    Added method strictCall() to \ppm\Compiler > ObjectHelpers

--- src/ppm/Utilities/ObjectHelpers.php
+++ src/ppm/Utilities/ObjectHelpers.php
@@ -49,5 +49,25 @@
             throw new MemberAccessException("Cannot write to an undeclared property $class::\$$name" . ($hint ? ", did you mean \$$hint?" : '.'));
         }
 
+        /**
+         * @param string $class
+         * @param string $method
+         * @param array $additionalMethods
+         * @throws ReflectionException
+         * @noinspection PhpUnused
+         */
+        public static function strictCall(string $class, string $method, array $additionalMethods = []): void
+        {
+            $hint = self::getSuggestion(array_merge(
+                get_class_methods($class),
+                self::parseFullDoc(new ReflectionClass($class), '~^[ \t*]*@method[ \t]+(?:\S+[ \t]+)??(\w+)\(~m'),
+                $additionalMethods
+            ), $method);
+
+            if (method_exists($class, $method)) { // called parent::$method()
+                $class = 'parent';
+            }
+            throw new MemberAccessException("Call to undefined method $class::$method()" . ($hint ? ", did you mean $hint()?" : '.'));
+        }
 
     }
