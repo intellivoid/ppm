10813b19ed41b4f8f62e62d2cf6fbca93e57514a
netkas <netkas@intellivoid.info>
2020-07-21 18:11:34 -0400

    Added method strictSet() to \ppm\Compiler > ObjectHelpers

--- src/ppm/Utilities/ObjectHelpers.php
+++ src/ppm/Utilities/ObjectHelpers.php
@@ -33,5 +33,21 @@
             throw new MemberAccessException("Cannot read an undeclared property $class::\$$name" . ($hint ? ", did you mean \$$hint?" : '.'));
         }
 
+        /**
+         * @param string $class
+         * @param string $name
+         * @throws ReflectionException
+         * @noinspection PhpUnused
+         */
+        public static function strictSet(string $class, string $name): void
+        {
+            $rc = new ReflectionClass($class);
+            $hint = self::getSuggestion(array_merge(
+                array_filter($rc->getProperties(ReflectionProperty::IS_PUBLIC), function ($p) { return !$p->isStatic(); }),
+                self::parseFullDoc($rc, '~^[ \t*]*@property(?:-write)?[ \t]+(?:\S+[ \t]+)??\$(\w+)~m')
+            ), $name);
+            throw new MemberAccessException("Cannot write to an undeclared property $class::\$$name" . ($hint ? ", did you mean \$$hint?" : '.'));
+        }
+
 
     }
