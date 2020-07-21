ae2fabdc1ffc51ac105c8c2abcc356ab86663b8e
netkas <netkas@intellivoid.info>
2020-07-21 18:11:06 -0400

    Added method strictGet() to \ppm\Compiler > ObjectHelpers

--- src/ppm/Utilities/ObjectHelpers.php
+++ src/ppm/Utilities/ObjectHelpers.php
@@ -4,8 +4,34 @@
 
     namespace ppm\Utilities;
 
+    use ppm\Exceptions\MemberAccessException;
+    use ReflectionClass;
+    use ReflectionException;
+    use ReflectionMethod;
+    use ReflectionProperty;
+    use Reflector;
 
+    /**
+     * Class ObjectHelpers
+     * @package ppm\Utilities
+     */
     final class ObjectHelpers
     {
+        /**
+         * @param string $class
+         * @param string $name
+         * @throws ReflectionException
+         * @noinspection PhpUnused
+         */
+        public static function strictGet(string $class, string $name): void
+        {
+            $rc = new ReflectionClass($class);
+            $hint = self::getSuggestion(array_merge(
+                array_filter($rc->getProperties(ReflectionProperty::IS_PUBLIC), function ($p) { return !$p->isStatic(); }),
+                self::parseFullDoc($rc, '~^[ \t*]*@property(?:-read)?[ \t]+(?:\S+[ \t]+)??\$(\w+)~m')
+            ), $name);
+            throw new MemberAccessException("Cannot read an undeclared property $class::\$$name" . ($hint ? ", did you mean \$$hint?" : '.'));
+        }
+
 
     }
