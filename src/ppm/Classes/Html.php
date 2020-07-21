1a3b6880a668f6c3ccb6f4b38614133ed6175060
netkas <netkas@intellivoid.info>
2020-07-21 19:14:51 -0400

    Added Html to \ppm\Classes

---
+++ src/ppm/Classes/Html.php
@@ -0,0 +1,10 @@
+<?php
+
+
+    namespace ppm\Classes;
+
+
+    class Html
+    {
+
+    }
--- src/ppm/ppm.php
+++ src/ppm/ppm.php
@@ -14,6 +14,7 @@
     include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Classes' . DIRECTORY_SEPARATOR . 'AutoIndexer.php');
     include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Classes' . DIRECTORY_SEPARATOR . 'FileSystem.php');
     include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Classes' . DIRECTORY_SEPARATOR . 'Finder.php');
+    include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Classes' . DIRECTORY_SEPARATOR . 'Html.php');
     include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Classes' . DIRECTORY_SEPARATOR . 'ObjectHelpers.php');
 
     include_once(__DIR__ . DIRECTORY_SEPARATOR . 'Exceptions' . DIRECTORY_SEPARATOR . 'AutoloaderException.php');
