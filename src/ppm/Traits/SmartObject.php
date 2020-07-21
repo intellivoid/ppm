d7897d942c467ec1af0054e82d55c4e2190fa806
netkas <netkas@intellivoid.info>
2020-07-21 18:20:42 -0400

    Added Builtin Call Method to SmartObject

--- src/ppm/Traits/SmartObject.php
+++ src/ppm/Traits/SmartObject.php
@@ -5,27 +5,45 @@
     namespace ppm\Traits;
 
 
+    use ppm\Exceptions\MemberAccessException;
+    use ppm\Exceptions\UnexpectedValueException;
+    use ppm\Utilities\ObjectHelpers;
+    use ReflectionException;
+
+    /**
+     * Trait SmartObject
+     * @package ppm\Traits
+     */
     trait SmartObject
     {
 
         /**
-         * @throws MemberAccessException
+         * @param string $name
+         * @param array $args
+         * @throws ReflectionException
          */
         public function __call(string $name, array $args)
         {
             $class = get_class($this);
 
-            if (ObjectHelpers::hasProperty($class, $name) === 'event') { // calling event handlers
+            if (ObjectHelpers::hasProperty($class, $name) === 'event')
+            { // calling event handlers
                 $handlers = $this->$name ?? null;
-                if (is_iterable($handlers)) {
-                    foreach ($handlers as $handler) {
+                if (is_iterable($handlers))
+                {
+                    foreach ($handlers as $handler)
+                    {
                         $handler(...$args);
                     }
-                } elseif ($handlers !== null) {
+                }
+                elseif ($handlers !== null)
+                {
                     throw new UnexpectedValueException("Property $class::$$name must be iterable or null, " . gettype($handlers) . ' given.');
                 }
 
-            } else {
+            }
+            else
+            {
                 ObjectHelpers::strictCall($class, $name);
             }
         }
