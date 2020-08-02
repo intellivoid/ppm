db592365f2cae794dd99f0855e6d7e706dfbc0a4
netkas <netkas@intellivoid.info>
2020-08-02 05:46:56 -0400

    Added ppm_version()

--- src/ppm/functions.php
+++ src/ppm/functions.php
@@ -1,13 +1,43 @@
 <?php
 
+    use ppm\Exceptions\AutoloaderException;
+    use ppm\Exceptions\InvalidComponentException;
+    use ppm\Exceptions\InvalidPackageLockException;
+    use ppm\Exceptions\PackageNotFoundException;
+    use ppm\Exceptions\VersionNotFoundException;
     use ppm\ppm;
 
     /**
+     * Imports a package using PPM
+     *
      * @param string $package
      * @param string $version
      * @return bool
+     * @throws AutoloaderException
+     * @throws InvalidComponentException
+     * @throws InvalidPackageLockException
+     * @throws PackageNotFoundException
+     * @throws VersionNotFoundException
      */
     function ppm_import(string $package, string $version="latest"): bool
     {
         return ppm::import($package, $version);
     }
+
+    /**
+     * Returns the definitions defined by PPM
+     *
+     * @return array
+     */
+    function ppm_definitions(): array
+    {
+        return array(
+            "PPM_VERSION" => PPM_VERSION,
+            "PPM_AUTHOR" => PPM_AUTHOR,
+            "PPM_URL" => PPM_URL,
+            "PPM_STATE" => PPM_STATE,
+            "PPM_INSTALL" => PPM_INSTALL,
+            "PPM_DATA" => PPM_DATA
+        );
+    }
+
