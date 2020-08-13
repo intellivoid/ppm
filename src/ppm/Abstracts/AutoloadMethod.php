6c6edadc308565759a5caff350a6b2addf8b22bb
netkas <netkas@intellivoid.info>
2020-08-12 22:11:16 -0400

    Updated PHPDoc for AutoloadMethod.php explaining the purpose of the autoloading method and the cons

--- src/ppm/Abstracts/AutoloadMethod.php
+++ src/ppm/Abstracts/AutoloadMethod.php
@@ -3,10 +3,28 @@
 
     namespace ppm\Abstracts;
 
-
+    /**
+     * Class AutoloadMethod
+     * @package ppm\Abstracts
+     */
     abstract class AutoloadMethod
     {
+        /**
+         * Auto loads all components static by order from the Components array in
+         * the package configuration file (package.json)
+         *
+         * This method can be slow for large packages and requires the component
+         * order to be correct otherwise the loading may fail
+         */
         const Static = "static";
 
+        /**
+         * Indexes the components during runtime with a cache-helper system and registers
+         * and auto loader for the imported package
+         *
+         * Fast during runtime but can often cause issues when importing packages dynamically
+         * due to an out-dated cache (if a lot of programs uses PPM)
+         */
         const Indexed = "indexed";
+
     }
